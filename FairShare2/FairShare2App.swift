4bfc24605d214e6e269da8e20c104423ecd46933
Aryan Bhargava <aryanb@umich.edu>
2024-07-27 19:54:14 +0800

    Initial Commit

---
+++ FairShare2/FairShare2App.swift
@@ -0,0 +1,17 @@
+//
+//  FairShare2App.swift
+//  FairShare2
+//
+//  Created by Aryan Bhargava on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FairShare2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
